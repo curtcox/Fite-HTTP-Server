30bf0083e49f4c45bbb80bd35519cef6c76ee9a4
Curt Cox <curt@Curts-MacBook-Pro.local>
2020-12-10 19:49:17 -0600

    Make handles final.

--- src/test/java/mite/HTTPRequestTest.java
+++ src/test/java/mite/HTTPRequestTest.java
@@ -1,17 +1,9 @@
 package mite;
 
 import org.junit.Test;
-import java.util.*;
 import static org.junit.Assert.*;
 
 public class HTTPRequestTest {
-
-    @Test
-    public void error_is_set_when_parsing_fails() {
-        String string = toString();
-        HTTPRequest request = parse(string);
-        assertTrue(request.error instanceof NoSuchElementException);
-    }
 
     @Test
     public void httpVersion_is_set_when_parsing_fails() {
